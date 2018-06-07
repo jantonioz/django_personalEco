8b062027e37de2eef1910e286a1c137cccb55753
1Postage <basictricks94@gmail.com>
2018-06-07 14:46:02 -0500

    ingreso

--- appPE/urls.py
+++ appPE/urls.py
@@ -3,7 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('usuarios', views.usuarios, name='usuarios'),
-    path('usuario/<str:username>', views.detalleUsuario, name='usuarioDetalle'),
+    path('', views.index, name="index"),
+
+    ## Usuario
+    path('usuarios', views.UsrView.usuarios, name='usuarios'),
+    path('usuario/<str:username>', views.UsrView.detalleUsuario, name='usuarioDetalle'),
+
+    ## Ingreso
+    path('ingresos', views.IngView.index, name="ingresos_index"),
+
+    ## Gasto
+    path('gastos', views.GastoView.index, name="gastos_index")
 
 ]
--- appPE/views.py
+++ appPE/views.py
@@ -2,13 +2,29 @@ from django.shortcuts import render
 from .models import Usuario, Ingreso, Gasto
 
 # Create your views here.
-def usuarios(request):
-    return render(request, 'Usuario/usuarios.html', 
-    {'usuarios': Usuario.objects.all()})
+def index(request):
+    top3_usrs = sorted(Usuario.objects.all(), key=lambda a : a.economia())[::-1]
+    top3_usrs = top3_usrs[:3]
+    return render(request, 'index.html', {'usuarios': top3_usrs})
 
-def detalleUsuario(request, username):
-    u = Usuario.objects.get(username=username)
-    ingresos = u.ingreso_set.all()
-    gastos = u.gasto_set.all()
-    return render(request, 'Usuario/detalle.html', 
-    {'usuario': u, 'ingresos' : ingresos, 'gastos': gastos})
+class GastoView():
+    def index(request):
+        g = Gasto.objects.all()
+        return render(request, 'Gasto/gastos.html', {'gastos': g})
+
+class IngView():
+    def index(request):
+        igs = Ingreso.objects.all()
+        return render(request, 'Ingreso/ingresos.html', {'ingresos': igs})
+
+class UsrView():
+    def usuarios(request):
+        return render(request, 'Usuario/usuarios.html', 
+        {'usuarios': Usuario.objects.all()})
+
+    def detalleUsuario(request, username):
+        u = Usuario.objects.get(username=username)
+        ingresos = u.ingreso_set.all()
+        gastos = u.gasto_set.all()
+        return render(request, 'Usuario/detalle.html', 
+        {'usuario': u, 'ingresos' : ingresos, 'gastos': gastos})
